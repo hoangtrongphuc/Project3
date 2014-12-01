4364472cf518e734b9185957a27ac65657826ade
nghiatv <nghiait0111@gmail.com>
2014-12-01 21:57:08 +0700

    xoa 1 vai ham

--- Source Code/ClientHTML5/room/index.php
+++ Source Code/ClientHTML5/room/index.php
@@ -48,94 +48,7 @@
     </div>
     <div id="content-list"> 
     
-     <table>
-    	<tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>	      
     
-    <tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>
-        <tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>
-        <tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>
-        <tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>
-        <tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>
-        <tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>
-        <tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>
-        <tr class="items">
-        	<td>
-            <div class="id">#12</div>
-            <div class="name">Nhao zo zo zo</div>
-            <div class="quantity">2</div>
-            <div class="money">10000</div>
-            <div class="password"> Không có</div>
-            </td>
-		</tr>
-        
-    
-    </table>
-
-
 	</div>
     <div id="bot-list">
     	<div id="page"> </div>
@@ -143,51 +56,15 @@
     </div>
   </div>
   <div id="friend">
-  <div id="list-name">Danh sách bạn bè</div>
-  <div id="list-fri">
-  <a href="#" class="fr" id="fri1">
-  	<div class="fri-name">Trần Huy</div>
-    <div class="fri-rank">245</div>
-  </a>
-   <a href="#" class="fr" id="fri2">
-  	<div class="fri-name">Lê Huy</div>
-    <div class="fri-rank">33</div>
-  </a>
-   <a href="#" class="fr" id="fri3">
-  	<div class="fri-name">Adam </div>
-    <div class="fri-rank">23</div>
-  </a>
-   <a href="#" class="fr" id="fri4">
-  	<div class="fri-name">Ando</div>
-    <div class="fri-rank">335</div>
-  </a>
-   <a href="#" class="fr" id="fri5">
-  	<div class="fri-name">Chesse</div>
-    <div class="fri-rank">185</div>
-  </a>
-  <a href="#" class="fr" id="fri5">
-  	<div class="fri-name">Chesse</div>
-    <div class="fri-rank">185</div>
-  </a>
-
-  <a href="#" class="fr" id="fri6">
-  	<div class="fri-name">Chesse</div>
-    <div class="fri-rank">185</div>
-  </a>
-
+    <div id="list-name">Danh sách bạn bè</div>
+    <div id="list-fri">
+  
   </div>
   
   
   </div>
   <div id="more">
     <a id="new" href="#data"><button type="button" class="btn btn-danger">Tạo bàn <span class="glyphicon glyphicon-plus-sign"></span></button></a>
-<style type="text/css">
-
-
-.fancybox-overlay fancybox-overlay-fixed
-{
-}
-</style>
     
     <div id="shop"><button type="button" class="btn btn-danger" disabled="disabled">Shop<small style="color:#CCC">(Chưa mở)</small></button></div>
     <div id="guide"><a href="/cotuong/?page=howtoplay"><button type="button" class="btn btn-danger">Hướng dẫn</button></a></div>
