35422aa3f9b1cd9509f9bdd330a1cc5b108e1a41
nghiatv <nghiait0111@gmail.com>
2014-12-14 16:47:03 +0700

    Tao moi footer cho trang web

--- Source Code/ClientHTML5/layout/footer.php
+++ Source Code/ClientHTML5/layout/footer.php
@@ -1,4 +1,51 @@
-  <?php $baseurl = 'http://localhost/cotuong'; ?>
-  <div id="footer">
-   	
-  </div>
+<footer class="footerpage">
+                <div class="grid-container">
+                    <div id="topagetop" class="scrollTop" title="Lên đầu trang"><a href="#wrapper">TOP</a></div>
+                    <div class="navbot floatleft">
+                        <div class="right-to-left">
+                            <ul class="navbot-grids">
+                                <li>
+                                    <span class="navbot_links"><a href="/huong-dan" title="Hướng dẫn chơi game trên Cotuongonline ">Hướng dẫn </a></span>
+                                    <span class="navbot_links"><a href="" title="Nạp thẻ">Nạp thẻ </a></span>
+                                    <span class="navbot_links"><a href="" title="Chưa mở">Mobile(Chưa mở) </a></span>
+                                </li>
+                                <li>
+                                    <span class="navbot_links"><a href="" title="Sự kiện">Sự kiện </a></span>
+                                    <span class="navbot_links"><a href="" title="Tin tức">Tin tức </a></span>
+                                    <span class="navbot_links"><a href="https://www.facebook.com/Congdong " title="Cộng đồng" target="_blank">Cộng đồng</a> </span>
+                                </li>
+                                <li>
+                                    <span class="navbot_links"><a href="/dich-vu" title="Dịch vụ">Dịch vụ</a> </span>
+                                    <span class="navbot_links"><a href="" title="Hỗ trợ" target="_blank">Hỗ trợ</a> </span>
+                                    <span class="navbot_links"><a href="" title="Hỏi đáp">Hỏi đáp</a> </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="contact-social floatleft">
+                        <div class="right-to-left">
+                            <div class="contact-social-icon">
+                                <a href="https://www.facebook.com/Congdong " class="facebook" title="Facebook  " target="_blank"</a>
+                                <a href="" title="Hỗ trợ" target="_blank" class="phone"></a>
+                            </div>
+                            <p class="supportcenter">Hỗ trợ: <strong>0964 407 993</strong></p>
+                        </div>
+                    </div>
+                    <div class="copyright floatleft">
+                        <div class="footerLogo">
+                            <a href="" target="_blank" ></a>
+                            <a href="" ></a>
+                        </div>
+                        <address>
+                            <span>Bản quyền trò chơi thuộc nhóm đồ án HTTT Bách Khoa</span><br>
+                            <span>Phòng 302, tầng 3 tòa nhà Tại chức - số 30 Lê Thanh Nghị - Hai Bà Trưng - Hà Nội</span>
+                        </address>
+                        <p class="footerlinks">
+                            <a href="" title="Giới thiệu về  ">Giới thiệu</a> -
+                            <a href="" title="Điều khoản sử dụng dịch vụ">Điều khoản</a> -
+                            <a href="" title="Hướng dẫn cài đặt và chơi game">Hướng dẫn cài đặt và chơi game</a>
+                        </p>
+                    </div>
+                    <div class="clrb"></div>
+                </div>
+            </footer>
